8a5008e52f450f0662ee011307ec1935dec9a84e
zuperm4n <zahid@zmdev.com>
2018-02-24 22:21:50 +0000

    gave myself some credits ;)

--- src/components/AppCredits.vue
+++ src/components/AppCredits.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <small>© <b><a href="https://twitter.com/jimmerioles" target="_blank" rel="noopener">Jim Merioles</a></b> {{ year }} - A <cite>freeCodeCamp</cite> Project</small>
+    <small>© <a href="https://twitter.com/jimmerioles" target="_blank" rel="noopener">Jim Merioles</a> - Forked by <a href="https://www.zahidmahmood.co.uk">Zahid Mahmood</a></small>
   </footer>
 </template>
 

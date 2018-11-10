5d64d9f680666b3bb03a6a11de9f6c938e7033c9
zuperm4n <zahid@zmdev.com>
2018-11-10 11:53:06 +0000

    wrapped original project in NativeScript

---
+++ app/components/AppCredits.vue
@@ -0,0 +1,23 @@
+<template>
+	<Label textWrap="true" textAlignment="center">
+		<FormattedString>
+			<Span text="Sourcecode available on GitHub/@zaarheed" />
+		</FormattedString>
+	</Label>
+</template>
+
+<script>
+export default {
+	name: "AppCredits",
+
+	props: {
+		year: {
+			type: Number,
+			required: true
+		}
+	}
+};
+</script>
+
+<style scoped>
+</style>
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    baseUrl: '/progressive-weather-app/',
-    pwa: {
-        themeColor: '#6CB9C8',
-        msTileColor: '#484F60'
-    }
-}
